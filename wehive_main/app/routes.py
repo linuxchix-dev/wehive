688b91ce78d0a42ee26794a07def9c4f259c2f05
JKP1008shine <sinjini2145@gmail.com>
2023-10-01 02:43:03 +0530

    added flash message in routes.py but yet not done the html designing for it

--- wehive_main/app/routes.py
+++ wehive_main/app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import SignupForm
 
@@ -7,7 +7,11 @@ from app.forms import SignupForm
 def index():
     return render_template('intro.html', title ='Wehive: Home')
 
-@app.route('/join')
+@app.route('/join', methods=['GET','POST'])
 def join():
     form = SignupForm()
+    if form.validate_on_submit ():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('join.html', title = 'Join Wehive', form=form)
+    

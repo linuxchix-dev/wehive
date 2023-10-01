166b82e757f0ef26b7ad13dcb24931ccbc7f8ccd
JKP1008shine <sinjini2145@gmail.com>
2023-10-01 11:49:23 +0530

    added email validator in form.py

--- wehive_main/app/forms.py
+++ wehive_main/app/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, EmailField
-from wtforms.validators import DataRequired, Length, EqualTo
+from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 class SignupForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=5)])
-    email = EmailField('Email address', validators=[DataRequired()])
+    email = EmailField('Email address', validators=[DataRequired(), Email(message='not an email')])
     new_password = PasswordField('Create a password', validators=[DataRequired(), Length(min=8)])
     confirm_password = PasswordField('Confirm the password', validators=[DataRequired(), EqualTo('new_password')])
     remember_me = BooleanField('Remember me')

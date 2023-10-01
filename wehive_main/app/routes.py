648cf9cde42873ca29adbc6f6c78e1b9ed281272
JKP1008shine <sinjini2145@gmail.com>
2023-10-01 15:45:25 +0530

    replaced the link generation with url_for method in .html files and routes.py

--- wehive_main/app/routes.py
+++ wehive_main/app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import SignupForm
 
@@ -12,6 +12,6 @@ def join():
     form = SignupForm()
     if form.validate_on_submit ():
         flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('join.html', title = 'Join Wehive', form=form)
     

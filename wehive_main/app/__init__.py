77f0794f23874b1790769f2a3ebede7d39551af5
JKP1008shine <sinjini2145@gmail.com>
2023-10-04 19:51:19 +0530

    imported sqlalchemy, created database and database migration instance in __init__.py, and SQLALCHEMY environment variables are set in config.py

--- wehive_main/app/__init__.py
+++ wehive_main/app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__) 
 app.config.from_object(Config)     
+db = SQLAlchemy(app)  #database instance#
+migrate = Migrate(app, db) #database migration instance#
 
-from app import routes     #this is to avoid circular imports
+from app import routes, models     #this is to avoid circular imports
 
 '''this __init__.py acts as the starting point of the application, Flask class
 which is imported from flask, the instance of the Flask class is our WSI application
